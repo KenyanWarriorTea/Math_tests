77748d36a3ca7a17082e20b48a9ca5c260c9e8b6
FleizeR1337 <220286@astanait.edu.kz>
2023-12-21 22:55:58 +0600

    bbb

--- testsite/urls.py
+++ testsite/urls.py
@@ -21,8 +21,13 @@ from django.urls import path
 from testsite import settings
 from bard.views import *
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bard.urls')),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

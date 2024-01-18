ca3c833983a940976aaa200203c29f6f3da41e48
FleizeR <lolkekheburek123@gmail.com>
2024-01-18 23:47:21 +0600

    web

--- bard/views.py
+++ bard/views.py
@@ -203,7 +203,7 @@ class LoginUser(DataMixin, LoginView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Авторизация / Авторландыру")
+        c_def = self.get_user_context(title="Авторизация")
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_success_url(self):

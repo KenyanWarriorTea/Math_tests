4b7a1c13e30b7da72d50c3f97a5771672bdcc230
FleizeR1337 <220286@astanait.edu.kz>
2024-01-22 10:50:37 +0600

    css

--- testsite/urls.py
+++ testsite/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bard.urls')),
     path('__nested_admin/', include('nested_admin.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
